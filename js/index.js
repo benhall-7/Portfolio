d05783cab19b192cb0758e87e9367784b0a0e2dd
BenHall-7 <benjaminjahall@gmail.com>
2019-11-11 22:59:47 -0500

    Working menu selection

--- js/index.js
+++ js/index.js
@@ -1,24 +1,34 @@
-let menu_classes = ['menu-about', 'menu-skills', 'menu-projects'];
-let data_classes = ['data-about', 'data-skills', 'data-projects'];
+const classes = ['about', 'skills', 'projects'];
 
 let menu = document.querySelector(".menu");
 let data = document.querySelector(".data");
 
 function select_from_menu(target) {
-    menu.childNodes.forEach(node => {
+    let target_category = classes.reduce((real, name) => {
+        if (real === undefined && target.classList.contains("menu-" + name)) {
+            return name;
+        } else {
+            return real;
+        }
+    }, undefined);
+    data_node = data.querySelector(".data-" + target_category);
+
+    Array.from(menu.children).forEach(node => {
         node.classList.remove("selected");
     });
+    Array.from(data.children).forEach(node => {
+        node.style.display = "none";
+    })
+    data_node.style.display = "block";
     target.classList.add("selected");
 }
 
-menu_classes.forEach(name => {
-    let query = "." + name;
+classes.forEach(name => {
+    let query = ".menu-" + name;
     menu.querySelector(query).addEventListener("click", ev => {
         select_from_menu(ev.target);
         ev.stopPropagation();
     });
 });
 
-document.addEventListener("load", ev => {
-
-});
+select_from_menu(document.querySelector(".menu-" + classes[0]));
