f25d2fc79772aa60eccf687649dd06e25a3aea3d
BenHall-7 <benjaminjahall@gmail.com>
2020-06-03 17:12:57 -0400

    Begin rewrite with working console input

--- js/index.js
+++ js/index.js
@@ -1,41 +1,159 @@
-let hidden;
-let data;
+let consoleInput = document.getElementById("console-input");
+const NBSP = "\u00A0";
 
-const ABOUT = 'ABOUT';
-const SKILLS = 'SKILLS';
-const PROJECTS = 'PROJECTS';
-const CURRENT_ID = "CURRENT_ID";
+class ConsoleHistory {
+    constructor() {
+        let storage = localStorage.getItem("console-history");
+        this.history = storage && JSON.parse(storage) || [];
+        this.historyIndex = null;
+    }
 
-const states = {
-  ABOUT: {},
-  SKILLS: {},
-  PROJECTS: {},
+    get() {
+        return this.history;
+    }
+
+    push(h) {
+        this.history.push(h);
+        this.historyIndex = null;
+    }
+
+    inc() {
+        if (this.historyIndex) {
+            this.clampIndex(this.historyIndex + 1);
+        }
+    }
+
+    dec() {
+        if (this.historyIndex) {
+            this.clampIndex(this.historyIndex - 1)
+        } else {
+            // automatically clamped to max value
+            this.clampIndex(Infinity);
+        }
+    }
+
+    clampIndex(index) {
+        if (this.history.length == 0) {
+            this.historyIndex = null;
+        } else {
+            this.historyIndex = Math.min(Math.max(0, index), this.history.length - 1);
+        }
+    }
+}
+
+let state = {
+    console: {
+        focused: true,
+        text: "",
+        position: 0,
+        history: new ConsoleHistory(),
+    },
 };
 
-const setData = contentID => {
-  while (data.lastChild) {
-    data.lastChild.remove();
-  }
-  data.appendChild(states[contentID]);
-  localStorage.setItem(CURRENT_ID, contentID);
+function setConsoleHtml() {
+    let text = state.console.text.replace(/ /g, NBSP);
+    let position = state.console.position;
+    // check if the cursor sits underneath a letter in the input string, or under a blank space
+    if (position < text.length) {
+        let before = text.slice(0, position);
+        let on = text[position];
+        let after = text.slice(position + 1);
+
+        let cursorElem = document.createElement("span");
+        cursorElem.append(on);
+        cursorElem.classList.add("console-cursor", "cursor-blink");
+
+        consoleInput.innerHTML = null;
+        consoleInput.append(before, cursorElem, after);
+    } else {
+        let cursorElem = document.createElement("span");
+        // non-breaking space (U-00A0). Can't use a regular " " character because HTML truncates it
+        cursorElem.append(NBSP);
+        cursorElem.classList.add("console-cursor", "cursor-blink");
+
+        consoleInput.innerHTML = null;
+        consoleInput.append(text, cursorElem);
+    }
+};
+
+function updateConsole() {
+    setConsoleHtml();
 }
 
-const setAboutMe = () => setData(ABOUT);
-const setSkills = () => setData(SKILLS);
-const setProjects = () => setData(PROJECTS);
+consoleInput.addEventListener("focus", e => {
+    console.log("focused");
+    state.console.focused = true;
+    let cursor = consoleInput.querySelector(".console-cursor");
+    cursor.classList.add("cursor-blink");
+});
 
-function loadData() {
-  hidden = document.getElementById("hidden");
-  data = document.getElementById("data");
+consoleInput.addEventListener("blur", e => {
+    console.log("blurred");
+    state.console.focused = false;
+    let cursor = consoleInput.querySelector(".console-cursor");
+    cursor.classList.remove("cursor-blink");
+});
 
-  states[ABOUT] = hidden.querySelector("#about");
-  states[SKILLS] = hidden.querySelector("#skills");
-  states[PROJECTS] = hidden.querySelector("#projects");
-  
-  const currentId = localStorage.getItem(CURRENT_ID);
-  if (currentId) {
-    setData(currentId);
-  } else {
-    setAboutMe();
-  }
-}
+consoleInput.addEventListener("keydown", e => {
+    switch (e.key) {
+        case "ArrowUp": {
+            if (e.shiftKey) {
+                state.console.history.dec();
+            } else {
+                state.console.position = 0;
+            }
+            updateConsole();
+            e.stopPropagation();
+            break;
+        }
+        case "ArrowDown": {
+            if (e.shiftKey) {
+                state.console.history.inc();
+            } else {
+                state.console.position = state.console.text.length;
+            }
+            updateConsole();
+            e.stopPropagation();
+            break;
+        }
+        case "ArrowLeft": {
+            state.console.position = Math.max(0, state.console.position - 1);
+            updateConsole();
+            e.stopPropagation();
+            break;
+        }
+        case "ArrowRight": {
+            state.console.position = Math.min(state.console.text.length, state.console.position + 1);
+            updateConsole();
+            e.stopPropagation();
+            break;
+        }
+        case "Backspace": {
+            let text = state.console.text;
+            let position = state.console.position;
+            if (position > 0) {
+                state.console.text = text.slice(0, position - 1) + text.slice(position);
+                state.console.position -= 1;
+                updateConsole();
+            }
+            e.stopPropagation();
+            break;
+        }
+        case "Delete": {
+            
+            updateConsole();
+            e.stopPropagation();
+            break;
+        }
+    }
+});
+
+consoleInput.addEventListener("keypress", e => {
+    state.console.text += e.key;
+    state.console.position += 1;
+    updateConsole();
+})
+
+consoleInput.addEventListener("submit", e => {
+    console.log("Submitted");
+})
--- js/projects.js
+++ js/projects.js
@@ -1,58 +1,58 @@
-projects = [
-    {
-        title: "Potluck Planner",
-        description: "Potluck Planner provides users a way to manage potlucks; see guests attending, what food they want to bring, and more.",
-        tech: [
-            "The 'home' and 'about' page of this project is designed simply through HTML, CSS, and some Javascript",
-            "This side of the project was my own work; early on, our team planned a set of styles to work with so we would stay consistent",
-            "The rest of the application is written with ReactJS."
-        ],
-        deployment: "https://potluck-planner-lambda.github.io/ui-ben/",
-        repo: "https://github.com/Potluck-Planner-Lambda"
-    },
-    {
-        title: "paracobNET",
-        description: "A 4-tool suite of programs allowing loading and editing of the proprietary '.prc' filetype in Super Smash Bros. Ultimate",
-        tech: [
-            "Currently I am the sole author of the project",
-            "Written in C#, and utilizing the .NET Core framework",
-            "Designed for portability as it can be used within other applications and on any computer architecture supported by .NET Core, like Windows, Linux, and MacOS."
-        ],
-        deployment: "https://github.com/BenHall-7/paracobNET/releases/latest",
-        repo: "https://github.com/BenHall-7/paracobNET"
-    }
-];
+// const projects = [
+//     {
+//         title: "Potluck Planner",
+//         description: "Potluck Planner provides users a way to manage potlucks; see guests attending, what food they want to bring, and more.",
+//         tech: [
+//             "The 'home' and 'about' page of this project is designed simply through HTML, CSS, and some Javascript",
+//             "This side of the project was my own work; early on, our team planned a set of styles to work with so we would stay consistent",
+//             "The rest of the application is written with ReactJS."
+//         ],
+//         deployment: "https://potluck-planner-lambda.github.io/ui-ben/",
+//         repo: "https://github.com/Potluck-Planner-Lambda"
+//     },
+//     {
+//         title: "paracobNET",
+//         description: "A 4-tool suite of programs allowing loading and editing of the proprietary '.prc' filetype in Super Smash Bros. Ultimate",
+//         tech: [
+//             "Currently I am the sole author of the project",
+//             "Written in C#, and utilizing the .NET Core framework",
+//             "Designed for portability as it can be used within other applications and on any computer architecture supported by .NET Core, like Windows, Linux, and MacOS."
+//         ],
+//         deployment: "https://github.com/BenHall-7/paracobNET/releases/latest",
+//         repo: "https://github.com/BenHall-7/paracobNET"
+//     }
+// ];
 
-let parent = document.querySelector("#projects");
+// let parent = document.querySelector("#projects");
 
-function createProject(data) {
-    let p = document.createElement("div");
+// function createProject(data) {
+//     let p = document.createElement("div");
 
-    let deployment = document.createElement("a");
-    let header = document.createElement("h1");
-    let desc = document.createElement("p");
-    let tech = document.createElement("ul");
-    let github = document.createElement("a");
+//     let deployment = document.createElement("a");
+//     let header = document.createElement("h1");
+//     let desc = document.createElement("p");
+//     let tech = document.createElement("ul");
+//     let github = document.createElement("a");
 
-    p.classList.add("project");
+//     p.classList.add("project");
 
-    deployment.textContent = data.title;
-    desc.textContent = data.description;
-    github.textContent = "Source";
+//     deployment.textContent = data.title;
+//     desc.textContent = data.description;
+//     github.textContent = "Source";
 
-    deployment.setAttribute("href", data.deployment);
-    github.setAttribute("href", data.repo);
+//     deployment.setAttribute("href", data.deployment);
+//     github.setAttribute("href", data.repo);
 
-    tech.append(...data.tech.map(point => {
-        let t = document.createElement("li");
-        t.textContent = point;
-        return t;
-    }));
+//     tech.append(...data.tech.map(point => {
+//         let t = document.createElement("li");
+//         t.textContent = point;
+//         return t;
+//     }));
     
-    header.append(deployment);
-    p.append(header, desc, tech, github);
+//     header.append(deployment);
+//     p.append(header, desc, tech, github);
 
-    return p;
-}
+//     return p;
+// }
 
-parent.append(...projects.map(proj => createProject(proj)));
+// parent.append(...projects.map(proj => createProject(proj)));
