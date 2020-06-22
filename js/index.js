d70fd988d087390991d275680dbe1fa74ed6e4a3
BenHall-7 <benjaminjahall@gmail.com>
2020-06-21 22:04:10 -0400

    Rewrite commands to use functional parsing

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let consoleMain = document.getElementById("console");
 let consoleWrapper = document.getElementById("console-wrapper");
+let content = document.getElementById("content");
 const NBSP = "\u00A0";
 
 // create a map of all the templates in the document by id
@@ -71,74 +72,47 @@ class ConsoleHistory {
     }
 }
 
+class Iterator {
+    constructor(array) {
+        this.inner = array;
+        this.index = 0;
+    }
+
+    current() {
+        if (this.index < this.inner.length) {
+            return this.inner[this.index];
+        } else {
+            return null;
+        }
+    }
+
+    next() {
+        this.index += 1;
+        return this.current();
+    }
+
+    upto() {
+        return this.inner.slice(0, this.index + 1).join(" ");
+    }
+}
+
 let state = {
     cmdHistory: new ConsoleHistory(),
 };
 
-async function handleCommand(commands) {
-    let content = document.getElementById("content");
-    content.innerHTML = null;
-
-    switch (commands[0]) {
-        case "": {
-            content.appendChild(templates["input-none"].content.cloneNode(true));
-            break;
-        }
-        case "help": {
-            content.appendChild(templates["input-help"].content.cloneNode(true));
-            break;
-        }
-        case "about": {
-            content.appendChild(templates["input-about"].content.cloneNode(true));
-            break;
-        }
-        case "contact": {
-            content.appendChild(templates["input-contact"].content.cloneNode(true));
-            break;
-        }
-        case "skills": {
-            content.appendChild(templates["input-skills"].content.cloneNode(true));
-            break;
-        }
-        case "projects": {
-            content.appendChild(templates["input-projects"].content.cloneNode(true));
-            break;
-        }
-        case "history": {
-            if (commands.length > 1) {
-                const second = commands[1];
-                if (second == "help") {
-                    content.appendChild(templates["input-history-help"].content.cloneNode(true));
-                } else if (second === "clear") {
-                    // since default behavior is to push new commands after this function exits
-                    // we send this callback to remove it later instead
-                    return () => {
-                        state.cmdHistory.clear();
-                        content.append("Command history cleared.");
-                    };
-                } else if (Number.isInteger(Number(second))) {
-                    let value = Number(second);
-                    if (value < 0)
-                        value += state.cmdHistory.length();
-                    if (value >= 0 && value < state.cmdHistory.length()) {
-                        let output = document.createElement("p");
-                        let text = state.cmdHistory.get(value);
-                        output.textContent = text;
-                        output.tabIndex = 0;
-                        output.classList.add("cmd");
-                        output.onclick = () => submitCmd(text);
-                        content.appendChild(output);
-                    } else {
-                        let output = document.createElement("p");
-                        output.textContent = "The specified index was not in the bounds of the history array";
-                        content.appendChild(output);
-                    }
-                } else {
-                    let response = templates["input-history-invalid"].content.cloneNode(true);
-                    response.querySelector(".arg0").append(commands[1]);
-                    content.appendChild(response);
-                }
-            } else {
+// object representation of commands for automatic input parsing
+// TODO: help text should be automatically generated for each command
+const COMMANDS = {
+    // if "commands" is an object, do a foreach loop
+    about: { template: "input-about" },
+    contact: { template: "input-contact" },
+    skills: { template: "input-skills" },
+    projects: { template: "input-projects" },
+    help: { template: "input-help" },
+    history: {
+        default: {
+            do: () => {
+                // TODO: find a way to turn this into a representative object
                 let header = document.createElement("h2");
                 header.textContent = "Command history"
                 let count = document.createElement("p");
@@ -158,25 +132,122 @@ async function handleCommand(commands) {
                 }
                 content.append(header, count, list);
             }
-            break;
+        },
+        // here the commands need to be an array because we have one that conditionally runs
+        // (based on if the input is a number)
+        commands: [
+            {
+                name: "clear",
+                template: "input-history-clear",
+                ret: () => state.cmdHistory.clear(),
+            }, {
+                conditional: arg => Number.isInteger(Number(arg)),
+                do: arg => {
+                    let value = Number(arg);
+                    if (value < 0)
+                        value += state.cmdHistory.length();
+                    if (value >= 0 && value < state.cmdHistory.length()) {
+                        let output = document.createElement("p");
+                        let text = state.cmdHistory.get(value);
+                        output.textContent = text;
+                        output.tabIndex = 0;
+                        output.classList.add("cmd");
+                        output.onclick = () => submitCmd(text);
+                        content.appendChild(output);
+                    } else {
+                        let output = document.createElement("p");
+                        output.textContent = "The specified index was not in the bounds of the history array";
+                        content.appendChild(output);
+                    }
+                }
+            },
+        ]
+    },
+}
+
+function findCommand(commands, arg) {
+    let find = null;
+    if (Array.isArray(commands)) {
+        for (const cmd of commands) {
+            if (cmd.name === arg) {
+                find = cmd;
+                break;
+            } else if (cmd.conditional && cmd.conditional(arg)) {
+                find = cmd;
+                break;
+            }
         }
-        default: {
-            let response = templates["input-invalid"].content.cloneNode(true);
-            response.querySelector(".arg0").append(commands[0]);
-            content.appendChild(response);
-            break;
+    } else {
+        for (const [name, cmd] of Object.entries(commands)) {
+            if (name === arg) {
+                find = cmd;
+                break;
+            }
         }
+    }
+    return find;
+}
+
+function doCommand(cmd, args) {
+    // to do: clarify if 'do' should come before or after 'template'
+    if (cmd.do)
+        cmd.do(args.current());
+
+    if (cmd.template) {
+        content.appendChild(templates[cmd.template].content.cloneNode(true));
+    }
+    
+    if (cmd.ret) {
+        return () => cmd.ret(args.current());
+    } else {
+        return null;
+    }
+}
+
+function recurseCommands(cmds, args) {
+    const cmd = findCommand(cmds, args.current());
+    if (cmd) {
+        if (cmd.commands) {
+            // if no further arguments are passed, there needs to be a default
+            if (!args.next()) {
+                if (cmd.default) {
+                    return doCommand(cmd.default, args);
+                } else {
+                    // error message for when no default is available
+                    let response = templates["input-default-invalid"].content.cloneNode(true);
+                    response.querySelector(".arg0").append(args.upto());
+                    content.appendChild(response);
+                }
+            } else {
+                return recurseCommands(cmd.commands, args);
+            }
+        } else {
+            return doCommand(cmd, args);
+        }
+    } else {
+        let response = templates["input-invalid"].content.cloneNode(true);
+        response.querySelector(".arg0").append(args.upto());
+        content.appendChild(response);
+    }
+}
+
+function handleCommand(args) {
+    content.innerHTML = null;
+    let iterator = new Iterator(args);
+    if (iterator.current()) {
+        return recurseCommands(COMMANDS, iterator);
+    } else {
+        content.appendChild(templates["input-none"].content.cloneNode(true));
     }
 }
 
 function submitCmd(commandStr) {
     let cmd = commandStr || consoleMain.value;
     // trim front and back, and split words up by spaces
-    handleCommand(cmd.trim().split(/ +/g)).then(cb => {
-        state.cmdHistory.push(cmd);
-        consoleMain.value = "";    
-        if (typeof cb == "function") cb();
-    });
+    let cb = handleCommand(cmd.trim().split(/ +/g));
+    state.cmdHistory.push(cmd);
+    consoleMain.value = "";
+    if (typeof cb == "function") cb();
 }
 
 consoleMain.addEventListener("keydown", e => {
