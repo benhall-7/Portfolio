6094296cb1196b3df25feb8cdcaa3e2ca8572200
BenHall-7 <benjaminjahall@gmail.com>
2020-12-20 17:15:04 -0500

    Make app display on html root

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,35 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+use yew::start_app;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+pub struct App;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub enum Msg {}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        App
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p>{ "Hello world!" }</p>
+        }
+    }
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, portfolio!");
+#[wasm_bindgen(start)]
+pub fn main() {
+    start_app::<App>();
 }
