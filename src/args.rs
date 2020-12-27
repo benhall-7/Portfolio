2bb9c286de0ed50dd0f3603e33d915831618f0f6
BenHall-7 <benjaminjahall@gmail.com>
2020-12-26 21:33:07 -0500

    Flesh out args; add back about page

--- src/args.rs
+++ src/args.rs
@@ -9,18 +9,51 @@ pub enum Args {
     Contact,
     Skills,
     Projects,
-    History,
+    #[structopt(about = "History of inputs to this terminal")]
+    History(HistoryArg),
     Diff,
-    Conway,
+    #[structopt(about = "Conway's game of life")]
+    Conway(ConwayArg),
+}
+
+#[derive(Debug, Clone, StructOpt)]
+pub struct HistoryArg {
+    #[structopt(subcommand)]
+    sub: Option<HistorySubCommand>,
+}
+
+#[derive(Debug, Clone, StructOpt)]
+pub enum HistorySubCommand {
+    #[structopt(about = "Clears all of this terminal's history")]
+    Clear,
+    #[structopt(about = "Gets the history of this terminal at a specific index.")]
+    Index {
+        num: i32
+    },
+}
+
+#[derive(Debug, Clone, StructOpt)]
+pub struct ConwayArg {
+    #[structopt(long, short)]
+    about: bool,
 }
 
 impl Args {
     pub fn view_content(&self) -> Html {
         match self {
             Args::About => {
-                html! { "This is the about page" }
+                html! { <>
+                    <div><img class="icon" src="img/self.jpg" alt="Photo of myself in front of pink blossoms"/></div>
+                    <p>
+                        {"Hi! My name is "}<span class="emph">{"Benjamin Hall"}</span>
+                        {",I'm a software engineer and full-stack web developer. Although born and raised in Northern California, I'm currently living near Pittsburgh, PA."}<br/><br/>
+                        {"As a lifelong advocate (and prior student) for Mathematics, I seek to find creative and generic solution to problems that not only improve consistency in user experience, but to ease future development too."}
+                    </p>
+                    // guarantees the containing div at least matches the height of the floating image
+                    <div style="clear:both;"></div>
+                </> }
             }
-            _ => html! { "This is a test" }
+            _ => html! { "" }
             // Args::Contact => {
 
             // }
